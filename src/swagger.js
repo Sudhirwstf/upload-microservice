8f9d58b87bb8132fb66a24f7a7ebd0b63af3d9df
sudhirwstf <sudhir.modanwal@thewasserstoff.com>
2025-07-28 12:57:39 +0530

    add backend url to swagger

--- src/swagger.js
+++ src/swagger.js
@@ -13,6 +13,9 @@ const options = {
       {
         url: "http://localhost:3000",
       },
+      {
+        url: process.env.BASE_URL || "https://api.uploads.wstf.tech",
+      },
     ],
     components: {
       securitySchemes: {
